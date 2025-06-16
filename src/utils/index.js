dc352ed910cc6cdf63663b591eb0fe55f1121eda
IStayThatWayCoding <istaythatway@gmail.com>
2025-06-15 22:12:45 -0400

    rules modification

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,84 +1,58 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 const rules = [
-    `Be respectful. Do not argue with, harass or bully any member of the discord. Any form of racism, sexism, homophobia, transphobia, etc. (including derogatory terms) will get you punished.`,
-    `Obey all staff members. Do not argue with staff - their decisions are final. If someone is breaking the rules, __ping a staff member or admin in the channel or dm a mod instead of mini-modding.__ Refrain from troll/unnecessary pinging of staff.`,
-    `Swearing is allowed, but please do not use such language excessively towards another user, this is a safe space.`,
-    `Do **__NOT__** post any inappropriate/NSFW content. This includes videos, images, links, emojis, profile pictures, usernames/nicknames, and discussion of overly-sexual content. Be mindful that there are younger members of the discord and that certain conversations may make people uncomfortable.`,
-    `Do not dox or post any irl personal information about anyone, or post any unsafe links such as IP grabbers or scams. **(Doing so will get you immediately banned from the server.)**`,
-    `Do not spam in channels and don't spam/mass ping people unnecessarily, especially other creators. **Do not ping Oasis (Swiftblade)** for any reason either (this also includes trying to @/everyone and spam reacting).`,
-    `Use the text channels in their correct use. For example, only use bot commands in <#903523421042929674>`,
-    `Do not discuss topics that tend to make people uncomfortable. This includes topics such as self-harm, intense political discussions and beliefs that would offend people, and extreme over-sexualization of individuals. This also applies to cheats/hack client naming for Minecraft, and other illegal practices such as account trading, etc.`,
-    `Do not be disruptive in voice channels. This includes spam hopping channels, being excessively loud/annoying, and playing soundboards.`,
-    `Use common sense! (Do not advertise/self promote, try and find loopholes, joke about being under 13, purposely confusing/upsetting others, etc.)`,
-    `Ban evasion and mute evasion are **__not allowed.__** If you are caught doing this, your main account's punishment will be extended.`,
-    `Do not discuss or ask about other members' punishments, and don't ping or make fun of those that are muted (other people’s bans, mutes, etc.)`,
-    `Please make sure you follow the Discord TOS and Community Guidelines. Failure to comply to these terms may get your account terminated!`
-]
+  `Treat everyone with kindness. Bullying, harassment, and discrimination are not allowed. This includes racism, sexism, homophobia, transphobia, and the use of slurs or offensive language.`,
+  `Staff members are here to help. If you disagree with a decision, reach out to a moderator privately in a respectful way. Public arguments with staff are not allowed. __Please avoid mini-modding and do not ping staff unless necessary.__`,
+  `Swearing is allowed, but it should not be directed at others or used aggressively. Keep conversations respectful for everyone.`,
+  `Do **__NOT__** post or share anything sexual, graphic, or otherwise inappropriate. This includes images, videos, links, usernames, nicknames, emojis, or messages. Remember that some members may be minors or uncomfortable with certain topics.`,
+  `Do not share your own or anyone else’s real-life personal information. Posting unsafe links, scams, or anything intended to collect personal data will result in an immediate ban.`,
+  `Do not spam messages, emojis, or pings. Avoid pinging large groups, other creators, or the server management unless it is truly important.`,
+  `Please keep each channel on topic. For example, use the <#903523421042929674> for bot commands. This helps keep the server organized.`,
+  `Do not start conversations that are likely to make others uncomfortable. This includes self-harm, intense political or religious debates, extreme sexual discussions, cheating or hacks in games, and illegal topics like account selling.`,
+  `Voice channels should be used responsibly. Do not spam join/leave, play loud noises, scream, or use soundboards in a disruptive way.`,
+  `Do not advertise your content or server without staff permission. Do not try to bend or find loopholes in the rules. Joking about being under 13 or trying to confuse others to avoid consequences is not allowed.`,
+  `Using alternate accounts to get around punishments is not allowed. Doing so will extend the punishment on your main account.`,
+  `Do not ask about or talk publicly about other members’ bans, mutes, or warnings. Do not make fun of or target muted members.`,
+  `Please make sure you follow the [Discord TOS](https://discord.com/terms) and [Community Guidelines](https://discord.com/guidelines). Failure to comply to these terms may get your account terminated!`,
+];
 
 module.exports = {
-    rule1: [
-        `${rules[0]}`
-    ],
-    rule2: [
-        `${rules[1]}`
-    ],
-    rule3: [
-        `${rules[2]}`
-    ],
-    rule4: [
-        `${rules[3]}`
-    ],
-    rule5: [
-        `${rules[4]}`
-    ],
-    rule6: [
-        `${rules[5]}`
-    ],
-    rule7: [
-        `${rules[6]}`
-    ],
-    rule8: [
-        `${rules[7]}`
-    ],
-    rule9: [
-        `${rules[8]}`
-    ],
-    rule10: [
-        `${rules[9]}`
-    ],
-    rule11: [
-        `${rules[10]}`
-    ],
-    rule12: [
-        `${rules[11]}`
-    ],
-    rule13: [
-        `${rules[12]}`
-    ],
+  rule1: [`${rules[0]}`],
+  rule2: [`${rules[1]}`],
+  rule3: [`${rules[2]}`],
+  rule4: [`${rules[3]}`],
+  rule5: [`${rules[4]}`],
+  rule6: [`${rules[5]}`],
+  rule7: [`${rules[6]}`],
+  rule8: [`${rules[7]}`],
+  rule9: [`${rules[8]}`],
+  rule10: [`${rules[9]}`],
+  rule11: [`${rules[10]}`],
+  rule12: [`${rules[11]}`],
+  rule13: [`${rules[12]}`],
 
-//     rules: [
-//         `
+  //     rules: [
+  //         `
 
-// > **Rule 1:** ${rules[0]}
-// > **Rule 2:** ${rules[1]}
-// > **Rule 3:** ${rules[2]}
-// > **Rule 4:** ${rules[3]}
-// > **Rule 5:** ${rules[4]}
-// > **Rule 6:** ${rules[5]}
+  // > **Rule 1:** ${rules[0]}
+  // > **Rule 2:** ${rules[1]}
+  // > **Rule 3:** ${rules[2]}
+  // > **Rule 4:** ${rules[3]}
+  // > **Rule 5:** ${rules[4]}
+  // > **Rule 6:** ${rules[5]}
 
-// `
-//     ],
+  // `
+  //     ],
 
-//     rules2: [
-//         `
-// > **Rule 7:** ${rules[6]}
-// > **Rule 8:** ${rules[7]}
-// > **Rule 9:** ${rules[8]}
-// > **Rule 10:** ${rules[9]}
-// > **Rule 11:** ${rules[10]}
-// > **Rule 12:** ${rules[11]}
-// > **Rule 13:** ${rules[12]}
-// *last updated: <t:${Math.round(new Date() / 1000)}:R>*`
-//     ]
-}
+  //     rules2: [
+  //         `
+  // > **Rule 7:** ${rules[6]}
+  // > **Rule 8:** ${rules[7]}
+  // > **Rule 9:** ${rules[8]}
+  // > **Rule 10:** ${rules[9]}
+  // > **Rule 11:** ${rules[10]}
+  // > **Rule 12:** ${rules[11]}
+  // > **Rule 13:** ${rules[12]}
+  // *last updated: <t:${Math.round(new Date() / 1000)}:R>*`
+  //     ]
+};

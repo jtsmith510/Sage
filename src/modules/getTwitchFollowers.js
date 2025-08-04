0fd40c18fe1d3f4de4a693ff6a0bf05a163eb2b3
IStayThatWayCoding <istaythatway@gmail.com>
2025-08-04 01:30:21 -0400

    Update getTwitchFollowers.js

--- src/modules/getTwitchFollowers.js
+++ src/modules/getTwitchFollowers.js
@@ -1,34 +1,46 @@
-const { readdirSync, readdir } = require('fs')
-const ExtendedClient = require('../class/ExtendedClient')
-const fetch = require('node-fetch').default;
-const axios = require('axios');
-const getRandomUA = require('../utils/getRandomUA');
+const { readdirSync, readdir } = require("fs");
+const ExtendedClient = require("../class/ExtendedClient");
+const fetch = require("node-fetch").default;
+const axios = require("axios");
+const getRandomUA = require("../utils/getRandomUA");
 
 /**
- * 
+ *
  * @param {ExtendedClient} bot
  */
 
 module.exports = (bot) => {
-    const guild = process.env.GUILD_ID
-    
+  const guild = process.env.GUILD_ID;
 
-    setInterval(async () => {
-        const { data } = await axios('https://decapi.net/twitch/followcount/itsoasislive', {
-            headers: {
-                "User-Agent": getRandomUA()
-            },
-        });
+  setInterval(async () => {
+    const { data } = await axios(
+      "https://decapi.net/twitch/followcount/itsoasisGG",
+      {
+        headers: {
+          "User-Agent": getRandomUA(),
+        },
+      }
+    );
 
-        function kFormatter(num){
-            const regExp = new RegExp('^-?\\d+(?:\.\\d{0,' + (1 || -1) + '})?');
-            return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000)).toString().match(regExp)[0] + 'K' : Math.sign(num) * Math.abs(num);
-        }
+    function kFormatter(num) {
+      const regExp = new RegExp("^-?\\d+(?:.\\d{0," + (1 || -1) + "})?");
+      return Math.abs(num) > 999
+        ? Math.sign(num) * (Math.abs(num) / 1000).toString().match(regExp)[0] +
+            "K"
+        : Math.sign(num) * Math.abs(num);
+    }
 
-        let followerCount = kFormatter(data);
+    let followerCount = kFormatter(data);
 
-        let vc = bot.channels.cache.get("1006433607222575155")
+    let vc = bot.channels.cache.get("1006433607222575155");
 
-        vc.setName(`Twitch: ${followerCount}`).catch(err => console.error(`${path.basename(__filename)} There was a problem changing the channel follower name: `, err))
-    }, 600000)
-}
+    vc.setName(`Twitch: ${followerCount}`).catch((err) =>
+      console.error(
+        `${path.basename(
+          __filename
+        )} There was a problem changing the channel follower name: `,
+        err
+      )
+    );
+  }, 600000);
+};

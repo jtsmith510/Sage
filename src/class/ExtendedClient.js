927251836283c1feacb47e69df43f2459707b8f8
IStayThatWayCoding <istaythatway@gmail.com>
2025-09-15 01:51:12 -0400

    disable twitch following count

--- src/class/ExtendedClient.js
+++ src/class/ExtendedClient.js
@@ -1,69 +1,72 @@
-const { Client, Partials, Collection, GatewayIntentBits, Discord, IntentsBitField } = require("discord.js");
+const {
+  Client,
+  Partials,
+  Collection,
+  GatewayIntentBits,
+  Discord,
+  IntentsBitField,
+} = require("discord.js");
 
-const commands = require('../handlers/commands');
-const events = require('../handlers/events');
-const config = require('../config')
-const deploy = require('../handlers/deploy')
+const commands = require("../handlers/commands");
+const events = require("../handlers/events");
+const config = require("../config");
+const deploy = require("../handlers/deploy");
 // const mongoose = require('../handlers/mongoose')
-const components = require('../handlers/components');
-const getTwitchFollowers = require('../modules/getTwitchFollowers');
-const getMemberCount = require('../modules/getMemberCount');
-const getSubCount = require('../modules/getSubCount');
+const components = require("../handlers/components");
+const getTwitchFollowers = require("../modules/getTwitchFollowers");
+const getMemberCount = require("../modules/getMemberCount");
+const getSubCount = require("../modules/getSubCount");
 
 module.exports = class extends Client {
-    collection = {
-        interactioncommands: new Collection(),
-        prefixcommands: new Collection(),
-        aliases: new Collection(),
-        components: {
-            modals: new Collection(),
-            autocomplete: new Collection(),
-            buttons: new Collection(),
-            selects: new Collection(),
-        }
+  collection = {
+    interactioncommands: new Collection(),
+    prefixcommands: new Collection(),
+    aliases: new Collection(),
+    components: {
+      modals: new Collection(),
+      autocomplete: new Collection(),
+      buttons: new Collection(),
+      selects: new Collection(),
+    },
+  };
+  applicationcommandsArray = [];
 
-    };
-    applicationcommandsArray = [];
+  constructor() {
+    super({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildVoiceStates,
+      ],
+      partials: [Partials.Channel, Partials.User],
+      // presence: {
+      //     activities: [{
+      //         name: 'custom',
+      //         type: 4,
+      //         state: `presence`,
+      //     }]
+      // }
+    });
+  }
 
-    constructor() {
-        super({
-            intents: [
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMembers,
-                GatewayIntentBits.GuildModeration,
-                GatewayIntentBits.GuildWebhooks,
-                GatewayIntentBits.GuildInvites,
-                GatewayIntentBits.GuildPresences,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.MessageContent,
-                GatewayIntentBits.DirectMessages,
-                GatewayIntentBits.GuildVoiceStates,
-            ],        
-            partials: [
-                Partials.Channel,
-                Partials.User
-            ],
-            // presence: {
-            //     activities: [{
-            //         name: 'custom',
-            //         type: 4,
-            //         state: `presence`,
-            //     }]
-            // }
-        });
-    };
+  start = async () => {
+    commands(this);
+    events(this);
+    components(this);
+    // getTwitchFollowers(this);
+    getMemberCount(this);
+    getSubCount(this);
 
-    start = async () => {
-        commands(this);
-        events(this);
-        components(this);
-        getTwitchFollowers(this);
-        getMemberCount(this);
-        getSubCount(this);
+    // mongoose();
+    await this.login(process.env.TOKEN);
 
-        // mongoose();
-        await this.login(process.env.TOKEN);
-        
-        deploy(this, config);
-    }
+    deploy(this, config);
+  };
 };
